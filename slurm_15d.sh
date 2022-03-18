87a7b0ae53cfea8e9db87623c64725e047d813a0
RI2 <kurt.29@osu.edu>
2022-03-18 17:38:51 -0400

    bug fix for slurm

--- slurm_15d.sh
+++ slurm_15d.sh
@@ -3,7 +3,7 @@
 cd ~
 source ~/anaconda3/etc/profile.d/conda.sh
 cd CAGNET
-conda activate cagnet
+source env.sh
 
 rank="$SLURM_PROCID"
 echo rank=$rank
@@ -13,7 +13,7 @@ echo root=$root
 hidden=$2
 replication=$3
 
-cmd="python -m torch.distributed.run --nproc_per_node=1 --nnodes=4 --node_rank=$rank --master_addr=$root --master_port=1234 gcn_distr_15d.py --accperrank=1 --epochs=10 --graphname=$graph --timing=True --midlayer=$hidden --runcount=1 --replication=$replication" 
+cmd="python -m torch.distributed.run --nproc_per_node=1 --nnodes=$SLURM_NTASKS --node_rank=$rank --master_addr=$root --master_port=1234 gcn_distr_15d.py --accperrank=1 --epochs=10 --graphname=$graph --timing=True --midlayer=$hidden --runcount=1 --replication=$replication  --normalization=True --activations=True --accuracy=True" 
 echo $cmd
 $cmd
 
--- slurm_tr.sh
+++ slurm_tr.sh
@@ -3,7 +3,7 @@
 cd ~
 source ~/anaconda3/etc/profile.d/conda.sh
 cd CAGNET
-conda activate cagnet
+source env.sh
 
 rank="$SLURM_PROCID"
 echo rank=$rank
@@ -12,7 +12,7 @@ root=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
 echo root=$root
 hidden=$2
 
-cmd="python -m torch.distributed.run --nproc_per_node=1 --nnodes=4 --node_rank=$rank --master_addr=$root --master_port=1234 gcn_distr_tr.py --accperrank=1 --epochs=10 --graphname=$graph --timing=True --midlayer=$hidden --runcount=1" 
+cmd="python -m torch.distributed.run --nproc_per_node=1 --nnodes=$SLURM_NTASKS --node_rank=$rank --master_addr=$root --master_port=1234 gcn_distr_tr.py --accperrank=1 --epochs=10 --graphname=$graph --timing=True --midlayer=$hidden --runcount=1 --normalization=True --activations=True --accuracy=True" 
 echo $cmd
 $cmd
 
