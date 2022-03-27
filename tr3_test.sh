fef0f7cd8b4601a6e3314571fcfe241128ec2bfa
RI2 <kurt.29@osu.edu>
2022-03-26 23:26:00 -0400

    new scheme slurm run added

---
+++ tr3_test.sh
@@ -0,0 +1,13 @@
+
+
+input=$1
+hidden=$2
+for mmorder in ssss sssd ssds ssdd sdss sdsd sdds sddd dsss dssd dsds dsdd ddss ddsd ddds dddd
+do
+    for rep in 1 2 4
+    do
+        cmd="srun slurm_tr3.sh $input $hidden $rep $mmorder"
+        echo $cmd
+        $cmd
+    done
+done
