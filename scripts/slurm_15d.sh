e1ef2c6a4872bc3c90d8d313ccc636b1e95b0d48
RI2 <kurt.29@osu.edu>
2022-04-10 04:36:22 -0400

    update for CAGNET 1.5D script

--- scripts/slurm_15d.sh
+++ scripts/slurm_15d.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+pwd=$(pwd)
 cd ~
 source ~/anaconda3/etc/profile.d/conda.sh
-cd CAGNET
+cd $pwd
 source env.sh
 
 rank="$SLURM_PROCID"
@@ -13,7 +14,7 @@ echo root=$root
 hidden=$2
 replication=$3
 
-cmd="python -m torch.distributed.run --nproc_per_node=1 --nnodes=$SLURM_NTASKS --node_rank=$rank --master_addr=$root --master_port=1234 gcn_distr_15d.py --accperrank=1 --epochs=10 --graphname=$graph --timing=True --midlayer=$hidden --runcount=1 --replication=$replication  --normalization=True --activations=True --accuracy=True" 
+cmd="python -m torch.distributed.run --nproc_per_node=1 --nnodes=$SLURM_NTASKS --node_rank=$rank --master_addr=$root --master_port=1234 src/gcn_distr_15d.py --accperrank=1 --epochs=10 --graphname=$graph --timing=True --midlayer=$hidden --runcount=1 --replication=$replication  --normalization=True --activations=True --accuracy=True" 
 echo $cmd
 $cmd
 
